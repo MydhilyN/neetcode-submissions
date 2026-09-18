11ac6b7738c1a64d496c090a308eb2a4db77446a
MydhilyN <mydhilyn@gmail.com>
2026-09-18 12:50:33 +0530

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n=max(nums)
+        for i in range(n+1):
+            if i not in nums:
+                return i
+
+        
